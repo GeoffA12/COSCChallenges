215b82990a6e88987d041d825113f2dc2128eb14
Geoff Arroyo <31530968+GeoffJ6@users.noreply.github.com>
2019-09-20 04:09:01 -0500

    Update IntegerUtils_Arroyo.java

--- utils/IntegerUtils_Arroyo.java
+++ utils/IntegerUtils_Arroyo.java
@@ -13,8 +13,7 @@ public interface IntegerUtils_Arroyo {
 	public static boolean isPrime(int k) {
 		if (k < 2) return false;
 		else if (k == 2 || k == 3) return true;
-		else if (k % 2 == 0 || k % 3 == 0) return false;
-		else if (Math.sqrt(k) % 1 == 0) return false;
+		else if (k % 2 == 0 || k % 3 == 0 || Math.sqrt(k) % 1 == 0) return false;
 		int modulusTest = 5;
 		while (modulusTest <= (k / 3)) {
 			if (k % modulusTest == 0) {
@@ -28,11 +27,12 @@ public interface IntegerUtils_Arroyo {
 	}
 	
 	public static int greatestCommonFactor(int m, int n) {
-		if (m == 0 && n == 0) {
-			throw new IllegalArgumentException("GCF of 0 and 0 is undefined!");
+		if (m == 0 && n == 0 || m == Integer.MIN_VALUE && n == Integer.MIN_VALUE) {
+			throw new IllegalArgumentException("GCF is undefined!");
 		}
-		else if (m == 0 && n != 0) return Math.abs(n);
-		else if (m != 0 && n == 0) return Math.abs(m);
+//		else if (m == 0 && n != 0) return Math.abs(n);
+//		else if (m != 0 && n == 0) return Math.abs(m);
+//		else if (m == n) return Math.abs(m);
 		int smaller;
 		int larger;
 		if (Math.abs(m) >= Math.abs(n)) {
@@ -48,20 +48,35 @@ public interface IntegerUtils_Arroyo {
 			larger = smaller;
 			smaller = temp % smaller;
 		}
+		if (larger == Integer.MIN_VALUE) {
+			throw new IllegalArgumentException("I can't represent Integer.MIN value in the positive domain of integers!");
+		}
 		return Math.abs(larger);
 	}
 	
 	// 7, 1
 	public static int getGreatestConstrainedMultiple(int k, int maximum) {
-		if (k == 0 && maximum < k) throw new IllegalArgumentException("Not valid");
+		if (k == 0 && maximum < k || k == Integer.MIN_VALUE) {
+			throw new IllegalArgumentException("Can't find a greatest constrained multiple with your input!");
+		}
+		
 		if (k == maximum || k == 0) return k;
-		int subtractAmount = ((maximum % k) + Math.abs(k)) % Math.abs(k);
-		return (maximum - subtractAmount);
+		int subtractAmount = (maximum % k); 
+		int f1 = subtractAmount + Math.abs(k);
+		int f2 = f1 % Math.abs(k);
+		int retValue = maximum - f2;
+		if ((Integer.signum(retValue) == -1 && Integer.signum(maximum) == 1) || (Integer.signum(retValue) == 1 && Integer.signum(maximum) == -1)) {
+			throw new IllegalArgumentException("Can't find a multiple.");
+		}
+		else {
+			return retValue;
+		}
 	}
 	
 	public static int getIntegerH(int h_q_3, int h_r_5) {
 		 if ((h_q_3 == 0 && h_r_5 != 0) || h_q_3 > (Integer.MAX_VALUE / 3) || h_q_3 < (Integer.MIN_VALUE / 3)) {
-			 throw new IllegalArgumentException("Invalid call!");
+			 //throw new IllegalArgumentException("Invalid call!");
+			 System.out.println("Case 1 failed.");
 		 }
 		 boolean needToDecrement = (h_q_3 < 0) ? true : false;
 		 int[] range = new int[3];
@@ -80,21 +95,23 @@ public interface IntegerUtils_Arroyo {
 				 return range[x]; 
 			 }
 		 }
-		 throw new IllegalArgumentException("Invalid call*!");
+		 System.out.println("No valid integer were found.");
+		 return -1;
+		 //throw new IllegalArgumentException("Invalid call*!");
 	}
 	
 	public static int getMaximum(Set<Integer> integerSet) {
-		if (integerSet.isEmpty() || integerSet.equals(null)) {
+		if (integerSet.isEmpty()) {
 			throw new IllegalArgumentException("You passed an empty set, so there's no possible maximum!");
 		}
-		return Collections.max(integerSet);
-		/*int max = Integer.MIN_VALUE;
+		// return Collections.max(integerSet);
+		int max = Integer.MIN_VALUE;
 		for (Integer i : integerSet) {
 			if (i > max) {
 				max = i;
 			}
 		}
-		return max;*/
+		return max;
 	}
 	
 	public static int getMinimum(int[] intArray) {
@@ -111,7 +128,6 @@ public interface IntegerUtils_Arroyo {
 	}
 	
 	public static boolean isSorted(int[] intArray) {
-		if (intArray.length < 2) return true;
 		for (int x = 1; x < intArray.length; ++x) {
 			if (intArray[x - 1] > intArray[x]) {
 				return false;
@@ -129,17 +145,43 @@ public interface IntegerUtils_Arroyo {
 	
 	public static int reverse(int k) {
 		if (k < 0) {
-			throw new IllegalArgumentException("Can't reverse a negative number!");
+			throw new IllegalArgumentException("Can't reverse negatives!");
 		}
-		String intString = String.valueOf(k);
+		String intString = Integer.toString(k);
+		char[] charArray = intString.toCharArray();
+		int start = 0; int end = charArray.length - 1;
+		while (start <= end) {
+			char temp = charArray[start];
+			charArray[start] = charArray[end];
+			charArray[end] = temp;
+			start++;
+			end--;
+		}
+		int[] maxChecker = new int[] {2, 1, 4, 7, 4, 8, 3, 6, 4, 7};
+		if (charArray.length == maxChecker.length) {
+			for (int x = 0; x < charArray.length; ++x) {
+				if (charArray[x] - '0' < maxChecker[x]) {
+					break;
+				}
+				else if (charArray[x] - '0' == maxChecker[x]) {
+					continue;
+				}
+				else {
+					throw new IllegalArgumentException("Uh oh! Integer overflow!");
+				}
+			}
+		}
+		return Integer.valueOf(new String(charArray));
+		/*String intString = String.valueOf(k);
 		StringBuilder sb = new StringBuilder();
 		Integer reverseInt = Integer.valueOf(sb.append(intString).reverse().toString());
+		
 		if (reverseInt <= Integer.MAX_VALUE) {
 			return reverseInt;
 		}
 		else {
 			throw new NumberFormatException("Number is too big!");
-		}
+		}*/
 		
 	}
 	
@@ -149,14 +191,7 @@ public interface IntegerUtils_Arroyo {
 		}
 		String longString = Long.toString(k);
 		long[] holder = new long[longString.length()];
-		/*int startVal;
-		if (longString.charAt(0) == '-') {
-			holder[0] = Long.valueOf(longString.substring(0, 2));
-			startVal = 2;
-		}
-		else {
-			startVal = 0;
-		}*/
+	
 		for (int x = 0; x < longString.length(); ++x) {
 			holder[x] = longString.charAt(x) - '0';
 		}
